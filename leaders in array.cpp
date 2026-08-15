4b968ca7e0d2eae83b536c3f1d3d0781db31a8ba
Vaishakh <vaishakh0102@gmail.com>
2026-08-15 12:00:08 +0530

    Brute Force Approach

--- leaders in array.cpp
+++ leaders in array.cpp
@@ -1,18 +1,19 @@
 class Solution {
   public:
     vector<int> leaders(vector<int>& arr) {
+      //Brute Force Approach - O(N^2) Time Complexity
         int n=arr.size();
         vector<int>ans;
         for (int i=0;i<n;i++){
             
-            bool Leader=true;
+            bool is_leader=true;
             for (int j=i+1;j<n;j++){
                 if (arr[j]>=arr[i]){
-                    Leader=false;
+                    is_leader=false;
                     break;
                 }
             }
-            if (Leader)ans.push_back(arr[i]);
+            if (is_leader)ans.push_back(arr[i]);
         }
         return ans;
         // code here
